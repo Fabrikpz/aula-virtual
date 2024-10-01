5f9d250632e8e1dcf633a7d25703c5e2a4e3a689
Fabrikpz <fabrisilveyra@gmail.com>
2024-10-01 11:33:59 -0300

    migración DB y actualización models.py para poder manejar estudiantes en los cursos

--- app/models.py
+++ app/models.py
@@ -2,11 +2,18 @@ from app import db
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
+estudiante_curso = db.Table('estudiante_curso',
+    db.Column('estudiante_id', db.Integer, db.ForeignKey('estudiante.id'), primary_key=True),
+    db.Column('curso_id', db.Integer, db.ForeignKey('curso.id'), primary_key=True)
+)
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(255))
+    
+    cursos = db.relationship('Curso', backref='user', lazy=True)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -19,3 +26,8 @@ class Curso(db.Model):
     nombre = db.Column(db.String(100), nullable=False)
     descripcion = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    estudiantes = db.relationship('Estudiante', secondary=estudiante_curso, backref=db.backref('cursos', lazy='dynamic'))
+
+class Estudiante(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
