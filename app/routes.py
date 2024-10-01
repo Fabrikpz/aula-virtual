9fad5dc5793d5f5eb10195f1d2b3232f7abd37cb
Juan Cruz Masante <d47708394@instituto.itr.edu.ar>
2024-10-01 08:54:25 -0300

    manejo de cursos

--- app/routes.py
+++ app/routes.py
@@ -4,19 +4,22 @@ from app.forms import RegistrationForm, LoginForm
 from app.models import User
 from flask_login import login_user, current_user, logout_user, login_required
 from flask import Blueprint
+from .models import Curso
+
 
 main = Blueprint('main', __name__)
 
 @main.route("/")
 def home():
-    return render_template("home.html")
+    cursos = Curso.query.all()
+    return render_template("home.html", cursos=cursos)
 
 @main.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
-        user.set_password(form.password.data)  # Hasheamos la contraseña aquí
+        user.set_password(form.password.data) 
         db.session.add(user)
         db.session.commit()
         flash('¡Tu cuenta ha sido creada!', 'success')
@@ -29,7 +32,7 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and user.check_password(form.password.data):
-            login_user(user)  # Agrega esta línea para autenticar al usuario
+            login_user(user)
             flash('¡Inicio de sesión exitoso!', 'success')
             return redirect(url_for('main.home'))
         else:
@@ -42,7 +45,49 @@ def logout():
     logout_user()
     return redirect(url_for('main.home'))
 
-@main.route("/crear_cursos")
-@login_required
+@main.route('/crear_cursos', methods=['GET', 'POST']) 
 def crear_cursos():
-    return render_template("cursos.html")
+    if request.method == 'POST':
+        curso_nombre = request.form.get('curso_nombre')
+        curso_descripcion = request.form.get('curso_descripcion')
+
+        if not curso_nombre or not curso_descripcion:
+            flash('Todos los campos son obligatorios', 'error')
+            return redirect(url_for('main.crear_cursos'))
+
+        nuevo_curso = Curso(nombre=curso_nombre, descripcion=curso_descripcion)
+        db.session.add(nuevo_curso)
+        db.session.commit()
+        
+        flash('Curso creado exitosamente', 'success')
+        return redirect(url_for('main.home'))
+
+    return render_template('cursos.html')
+
+@main.route('/eliminar_curso/<int:curso_id>', methods=['POST'])
+@login_required 
+def eliminar_curso(curso_id):
+    curso = Curso.query.get_or_404(curso_id) 
+    db.session.delete(curso) 
+    db.session.commit()  
+    flash('Curso eliminado exitosamente', 'success')
+    return redirect(url_for('main.home')) 
+
+@main.route('/actualizar_curso/<int:curso_id>', methods=['GET', 'POST'])
+@login_required  
+def actualizar_curso(curso_id):
+    curso = Curso.query.get_or_404(curso_id) 
+
+    if request.method == 'POST':
+        curso.nombre = request.form.get('curso_nombre')
+        curso.descripcion = request.form.get('curso_descripcion')
+
+        if not curso.nombre or not curso.descripcion:
+            flash('Todos los campos son obligatorios', 'error')
+            return redirect(url_for('main.actualizar_curso', curso_id=curso_id))
+
+        db.session.commit() 
+        flash('Curso actualizado exitosamente', 'success')
+        return redirect(url_for('main.home'))
+
+    return render_template('actualizar_curso.html', curso=curso)
